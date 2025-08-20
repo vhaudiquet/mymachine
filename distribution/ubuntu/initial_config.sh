6da948932c8dc10cdba2646d7a9ec1a13209100f
vhaudiquet <vhaudiquet343@hotmail.fr>
2025-08-20 17:35:24 +0200

    ubuntu: Fix disable unwanted extension using sudo

--- distribution/ubuntu/initial_config.sh
+++ distribution/ubuntu/initial_config.sh
@@ -2,7 +2,7 @@
 
 disable_unwanted_extensions() {
     # Disable default Ubuntu gnome extensions
-    gnome-extensions disable ding@rastersoft.com # Desktop Icons
+    sudo -u ${USERNAME} gnome-extensions disable ding@rastersoft.com # Desktop Icons
 }
 
 WHEEL_GROUP="sudo"
